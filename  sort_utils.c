14d3c01ca95f91eedf52da5f3d74d372e3609a15
Mariouch Mohamed <mariouchdev@icloud.com>
2026-04-22 18:17:29 +0200

    start sort

---
+++  sort_utils.c
@@ -0,0 +1,48 @@
+#include "push_swap.h"
+
+int get_min(t_stack *stack)
+{
+	int min;
+
+	if (!stack)
+		return (0);
+	
+	min = stack->value;
+	while(stack)
+	{
+		if (stack->value < min)
+			min = stack->value;
+		stack = stack->next;
+	}
+	return (min);
+}
+
+int get_max(t_stack *stack)
+{
+	int max;
+
+	if (!stack)
+		return (0);
+	max = stack->value;
+	while(stack)
+	{
+		if (stack->value > max)
+			max = stack->value;
+		stack = stack->next;
+	}
+	return (0);
+}
+
+int get_position(t_stack *stack, int value)
+{
+	int pos;
+	pos = 0;
+	while (stack)
+	{
+		if(stack->value == value)
+			return (pos);
+		pos++;
+		stack = stack->next;
+	}
+	return (-1);
+}
